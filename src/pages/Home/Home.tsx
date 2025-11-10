b7985359581e0566a51e13436c2f715fc450d612
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2025-11-10 09:59:43 -0500

    Set up fonts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import './Home.scss'
+import me from '@/assets/me.jpg'
 
 export default function Home() {
     return (
@@ -7,16 +8,16 @@ export default function Home() {
                 <h1 className="home__title">Marichka Offen</h1>
                 <p className="home__description">One of my deepest joys comes from turning messy UI into effortless UX that feels like second nature to users.</p>
                 <div className="home__profile">
-                    <img src="/profile.jpg" alt="Profile" className="home__profile-image" />
+                    <img className="home__profile-image" src={me} alt="Profile" />
                     <div className="home__profile-info">
-                        <p className="home__profile-role">Front end engineer</p>
-                        <p className="home__profile-location">Web developer</p>
+                        <h2 className="home__profile-role">Front end engineer</h2>
+                        <h2 className="home__profile-location">Web developer</h2>
                     </div>
                 </div>
             </div>
             <div className="home__column-right">
                 <div className="home__useful-links">
-                    <h3>Where you can start</h3>
+                    <h3 className="home__useful-links-title">Where you can start</h3>
                     <ul>
                         <li><a href="#" className="home__link">Learn fun facts about me</a></li>
                         <li><a href="#" className="home__link">Browse my work history</a></li>
